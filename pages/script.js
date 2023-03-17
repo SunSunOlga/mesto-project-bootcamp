90e5171f96437609847f7d1548db4873b3d4f5eb
Olga Senkina <kasta95_12@mail.ru>
2023-03-17 23:46:45 +0300

    form and js

--- pages/script.js
+++ pages/script.js
@@ -3,8 +3,11 @@ const buttonEdit = document.querySelector('.js-edit-profile');
 const buttonAdd = document.querySelector('.profile__button-add');
 const buttonLike = document.querySelector('.js-like-element');
 const buttonClose = document.querySelector('.js-close-form');
-const formBlock = document.querySelector('.popup');
 const inputForm = document.querySelector('.form__field');
+const blockPopup = document.querySelector('.popup');
+const buttonSave = document.querySelector('.js-save-form');
+
+
 
 
 
@@ -18,37 +21,40 @@ buttonLike.classList.add('button-like_active');
 */
 
 function VisibleForm() {
-  formBlock.classList.add('popup_opened');
+  blockPopup.classList.add('popup_opened');
 }
 buttonEdit.addEventListener('click', VisibleForm);
 
-
-
-function HiddenForm(evt) {
-  formBlock.classList.remove('popup_opened');
-  const basikForm = document.querySelector('.js-form');
-  basikForm.reset();
+function closeForm() {
+  blockPopup.classList.remove('popup_opened');
+  blockForm.reset();
 }
-buttonClose.addEventListener('click', HiddenForm )
+buttonClose.addEventListener('click', closeForm )
 
 
-
-
-/*
-const nameInput = document.querySelector('.js-input-name');
-const jobInput = document.querySelector('.js-input-job');
- const basikForm = document.querySelector('.js-form');
-const buttonSave = document.querySelector('.js-save-form');
- const itemInput = document.querySelector('.form__field');
-
+const blockForm = blockPopup.querySelector('.js-form');
+const nameInput = blockForm.querySelector('.js-input-name');
+const jobInput = blockForm.querySelector('.js-input-job');
 
 function addForm(evt) {
   evt.preventDefautlt();
-  nameInput.textContent = input.value;
-  jobInput.textContent = input.value;
+
+  nameInput.setAttribute('value');
+  jobInput.setAttribute('value');
+
+  const NewItemName = nameProfile.cloneNode(true);
+  const NewItemJob = jobProfile.cloneNode(true);
+  const nameProfile = document.querySelector('.js-name-profile');
+  const jobProfile = document.querySelector('.js-job-profile');
+
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+
+  nameProfile.append(NewItemName);
+  jobProfile.append(NewItemJob);
 
 }
 
 buttonSave.addEventListener('click', addForm)
-*/
+
 
