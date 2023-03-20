b34472586404092171ef5c2d7f7e4528b976b36b
Olga Senkina <kasta95_12@mail.ru>
2023-03-20 18:50:36 +0300

    caaards

--- pages/script.js
+++ pages/script.js
@@ -7,23 +7,12 @@ const inputForm = document.querySelector('.form__field');
 const blockPopup = document.querySelector('.popup');
 const buttonSave = document.querySelector('.js-save-form');
 
-
-
-
-
-
-//кнопка лайк меняет цвет
-/*
-buttonLike.addEventListener('click', PushLike)
-function PushLike() {
-buttonLike.classList.add('button-like_active');
-}
-*/
-
-function VisibleForm() {
+function visibleFormProfile() {
   blockPopup.classList.add('popup_opened');
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
-buttonEdit.addEventListener('click', VisibleForm);
+buttonEdit.addEventListener('click', visibleFormProfile);
 
 function closeForm() {
   blockPopup.classList.remove('popup_opened');
@@ -35,26 +24,95 @@ buttonClose.addEventListener('click', closeForm )
 const blockForm = blockPopup.querySelector('.js-form');
 const nameInput = blockForm.querySelector('.js-input-name');
 const jobInput = blockForm.querySelector('.js-input-job');
+const nameProfile = document.querySelector('.js-name-profile');
+const jobProfile = document.querySelector('.js-job-profile');
 
-function addForm(evt) {
-  evt.preventDefautlt();
 
-  nameInput.setAttribute('value');
-  jobInput.setAttribute('value');
-
-  const NewItemName = nameProfile.cloneNode(true);
-  const NewItemJob = jobProfile.cloneNode(true);
-  const nameProfile = document.querySelector('.js-name-profile');
-  const jobProfile = document.querySelector('.js-job-profile');
+function handleFormSubmit(evt) {
+  evt.preventDefault();
 
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-
-  nameProfile.append(NewItemName);
-  jobProfile.append(NewItemJob);
-
+  closeForm();
 }
 
-buttonSave.addEventListener('click', addForm)
+blockForm.addEventListener('submit', handleFormSubmit);
+
+
+ const cardsPopup = document.querySelector('.popup-cards');
+ const buttonCloseCard = document.querySelector('.js-close-cards');
+ const formCards = cardsPopup.querySelector('.js-form-cards');
+function VisibleFormCards () {
+  cardsPopup.classList.add('popup_opened');
+}
+buttonAdd.addEventListener('click', VisibleFormCards);
+
+function cardClose () {
+  cardsPopup.classList.remove('popup_opened');
+  formCards.reset();
+}
+buttonCloseCard.addEventListener('click', cardClose);
+
+
+const initialCards = [
+  {
+    name: 'Архыз',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg'
+  },
+  {
+    name: 'Челябинская область',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg'
+  },
+  {
+    name: 'Иваново',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/ivanovo.jpg'
+  },
+  {
+    name: 'Камчатка',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kamchatka.jpg'
+  },
+  {
+    name: 'Холмогорский район',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/kholmogorsky-rayon.jpg'
+  },
+  {
+    name: 'Байкал',
+    link: 'https://pictures.s3.yandex.net/frontend-developer/cards-compressed/baikal.jpg'
+  }
+];
+
+ const cardItem = document.querySelector('.element');
+ //const cardNewElement = cardItem.cloneNode(true);
+ const itemSection = document.querySelector('.elements-grid');
+
+const itemForm = document.querySelector('.js-form-cards');
+const buttonAddCard = itemForm.querySelector('.js-save-card');
+const inputCardName = itemForm.querySelector('.js-input-text-card');
+const inputCardLink = itemForm.querySelector('.js-input-link-card');
+const itemTemplate = document.querySelector('.item-template').content.querySelector('.element');
+
+function createItem() {
+const newItemCard = itemTemplate.cloneNode(true);
+const itemName = newItemCard.querySelector('.element__caption');
+const ItemPicture = newItemCard.querySelector('.element__picture');
+itemName.textContent = inputCardName.value;
+ItemPicture
+return newItemCard;
+}
+
+function addItem(newItem, section){
+ section.append(newItem);
+}
+
+itemForm.addEventListener('submit', function (event) {
+  event.preventDefault();
+  const newItem = createItem();
+  addItem(newItem, itemSection);
+});
+
+//itemSection.append(cardNewElement);
+
+
+
 
 
